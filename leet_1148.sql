bd831b89d159671097d1f30c0d56fc11cc9d9b59
Pooja BK <103930266+poojabk9@users.noreply.github.com>
2023-07-02 17:57:51 +0200

    Add files via upload

---
+++ leet_1148.sql
@@ -0,0 +1,9 @@
+--- 1148. Article Views I ---
+
+-- Write an SQL query to find all the authors that viewed at least one of their own articles. --
+-- Return the result table sorted by id in ascending order. --
+
+SELECT DISTINCT(author_id) AS id
+FROM Views
+WHERE author_id = viewer_id
+ORDER By id
